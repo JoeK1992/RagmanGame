4b40b088b3f50f330a584fed58e70aeccbd9cd61
Joe Kelly <jpk17@hotmail.co.uk>
2021-06-24 22:54:31 +0100

    Words and definitions generating

--- screens/CountdownScreen.js
+++ screens/CountdownScreen.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import { Text, View } from "react-native";
 import styles from "../styles";
+import { useNavigation } from "@react-navigation/native";
 
 export default function CountdownScreen(props) {
+  const navigation = useNavigation();
   const { playerName } = props.route.params;
 
   const [counter, setCounter] = useState(5);
 
   React.useEffect(() => {
+    if (counter === 1) {
+      setInterval(() => setCounter("Go!"), 1000);
+    }
     const timer =
-      counter === 0
-        ? navigation.navigate("Game", { playerName })
+      counter === "Go!"
+        ? setInterval(() => navigation.navigate("Game", { playerName }), 1000)
         : setInterval(() => setCounter(counter - 1), 1000);
     return () => clearInterval(timer);
   }, [counter]);
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,48 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import styles from "../styles";
+import apiKey from "../apiKey";
+import { render } from "react-dom";
 
-export default function GameScreen() {
-  return <Text>Hello</Text>;
+class GameScreen extends React.Component {
+  state = {
+    word: "",
+    definition: "",
+  };
+
+  getData(length) {
+    fetch(
+      `http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=20&minLength=${length}&maxLength=5&limit=1&api_key=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ word: data[0].word }))
+      .then(() => {
+        fetch(
+          `https://api.wordnik.com/v4/word.json/${this.state.word}/definitions?limit=1&includeRelated=false&useCanonical=false&includeTags=false&api_key=${apiKey}`
+        )
+          .then((response) => response.json())
+          .then((definition) => {
+            const filteredDefinition = definition[0].text.replace(
+              / \([\s\S]*?\)/g
+            );
+
+            this.setState({ definition: filteredDefinition });
+          });
+      });
+  }
+
+  componentDidMount() {
+    this.getData(5);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.word}</Text>
+        <Text>{this.state.definition}</Text>
+      </View>
+    );
+  }
 }
+
+export default GameScreen;
