f4266b5e858ae7c84b4d5aa41e3da83a990a2fdd
Joe Kelly <jpk17@hotmail.co.uk>
2021-06-29 13:13:06 +0100

    Words scrambled

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import styles from "../styles";
 import apiKey from "../apiKey";
-import { render } from "react-dom";
 
 class GameScreen extends React.Component {
   state = {
     word: "",
     definition: "",
+    scrambledWord: "",
   };
 
   getData(length) {
@@ -15,7 +15,14 @@ class GameScreen extends React.Component {
       `http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=20&minLength=${length}&maxLength=5&limit=1&api_key=${apiKey}`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ word: data[0].word }))
+      .then((data) => {
+        const scrambledWord = data[0].word
+          .split("")
+          .sort(() => 0.5 - Math.random())
+          .join("");
+        this.setState({ word: data[0].word, scrambledWord: scrambledWord });
+      })
+
       .then(() => {
         fetch(
           `https://api.wordnik.com/v4/word.json/${this.state.word}/definitions?limit=1&includeRelated=false&useCanonical=false&includeTags=false&api_key=${apiKey}`
@@ -38,7 +45,7 @@ class GameScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>{this.state.word}</Text>
+        <Text>{this.state.scrambledWord}</Text>
         <Text>{this.state.definition}</Text>
       </View>
     );
